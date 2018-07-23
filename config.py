053c5ea935594e665b51515084936776cf7bcd8f
dinocoder <38384146+dinocoder@users.noreply.github.com>
2018-07-23 13:25:45 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+token = 'NDY5OTc4NDY1NzE0NzAwMjk4.DjUUAw.-9SoQSPby8xwJuzLqTHrIqdC3RA'
+
+admin_roles = [
+'Temporary Admin', 
+'Godly Administrator', 
+'Server Designer And Co-Owner', 
+'Owner (GamerDanielRB_YT)'
+]
+
+announcement_channel = 'uplodad-announcements'
+
+prefix = '*'
